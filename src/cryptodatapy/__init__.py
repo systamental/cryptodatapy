523246a2ee706c3967eea80d10d331c13ad193e7
Nick Latinovic <nicklatinovic@gmail.com>
2022-06-28 00:36:40 -0500

    build: updatec init file

--- src/cryptodatapy/__init__.py
+++ src/cryptodatapy/__init__.py
@@ -1,3 +1,10 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("cryptodatapy")
+
+
+from cryptodatapy import(
+util,
+data_requests,
+data_vendors
+)

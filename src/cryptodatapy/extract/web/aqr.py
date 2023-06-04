fbafe7d8a330f476b0b4444125594a7c30f92c93
Nick Latinovic <nicklatinovic@gmail.com>
2023-06-03 23:54:21 -0300

    feat: added web base class and aqr data source

---
+++ src/cryptodatapy/extract/web/aqr.py
@@ -0,0 +1,301 @@
+import logging
+from typing import Dict, List, Optional, Union
+
+import pandas as pd
+
+from cryptodatapy.extract.datarequest import DataRequest
+from cryptodatapy.extract.web.web import Web
+from cryptodatapy.transform.convertparams import ConvertParams
+from cryptodatapy.transform.wrangle import WrangleData
+from cryptodatapy.util.datacredentials import DataCredentials
+
+# data credentials
+data_cred = DataCredentials()
+
+
+class AQR(Web):
+    """
+    Retrieves data from AQR data sets.
+    """
+
+    def __init__(
+            self,
+            categories=None,
+            indexes: Optional[Dict[str, List[str]]] = None,
+            assets: Optional[Dict[str, List[str]]] = None,
+            markets: Optional[Dict[str, List[str]]] = None,
+            market_types=None,
+            fields: Optional[Dict[str, List[str]]] = None,
+            frequencies=None,
+            base_url: str = data_cred.aqr_base_url,
+            file_formats: Optional[Union[str, List[str]]] = 'xlsx'
+    ):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        categories: list or str, {'crypto', 'fx', 'rates', 'eqty', 'cmdty', 'credit', 'macro', 'alt'}
+            List or string of available categories, e.g. ['crypto', 'fx', 'alt'].
+        indexes: dictionary, optional, default None
+            Dictionary of available indexes, by cat-indexes key-value pairs,  e.g. [{'eqty': ['SPX', 'N225'],
+            'rates': [.... , ...}.
+        assets: dictionary, optional, default None
+            Dictionary of available assets, by cat-assets key-value pairs,  e.g. {'rates': ['Germany 2Y', 'Japan 10Y',
+            ...], 'eqty: ['SPY', 'TLT', ...], ...}.
+        markets: dictionary, optional, default None
+            Dictionary of available markets, by cat-markets key-value pairs,  e.g. [{'fx': ['EUR/USD', 'USD/JPY', ...],
+            'crypto': ['BTC/ETH', 'ETH/USDT', ...}.
+        market_types: list
+            List of available market types e.g. [spot', 'perpetual_future', 'future', 'option'].
+        fields: dictionary, optional, default None
+            Dictionary of available fields, by cat-fields key-value pairs,  e.g. {'cmdty': ['date', 'open', 'high',
+            'low', 'close', 'volume'], 'macro': ['actual', 'previous', 'expected', 'surprise']}
+        frequencies: dictionary
+            Dictionary of available frequencies, by cat-frequencies key-value pairs, e.g. {'fx':
+            ['d', 'w', 'm', 'q', 'y'], 'rates': ['d', 'w', 'm', 'q', 'y'], 'eqty': ['d', 'w', 'm', 'q', 'y'], ...}.
+        base_url: str, optional, default None
+            Base url used for GET requests. If not provided, default is set to base_url stored in DataCredentials.
+        file_formats: list or str, {'xlsx', 'xls'}, default 'xlsx'
+            List of available file formats.
+        """
+        Web.__init__(
+            self,
+            categories,
+            indexes,
+            assets,
+            markets,
+            market_types,
+            fields,
+            frequencies,
+            base_url,
+            file_formats
+        )
+
+        if categories is None:
+            self.categories = ["fx", "rates", "eqty", "cmdty", "credit"]
+        if frequencies is None:
+            self.frequencies = {
+                "fx": ["m", "q", "y"],
+                "rates": ["m", "q", "y"],
+                "cmdty": ["m", "q", "y"],
+                "eqty": ["d", "w", "m", "q", "y"],
+                "credit": ["m", "q", "y"],
+            }
+        if market_types is None:
+            self.market_types = ["spot", "future"]
+        if fields is None:
+            self.fields = self.get_fields_info()
+
+    def get_indexes_info(self) -> None:
+        """
+        Get indexes info.
+        """
+        eqty_idxs = ['AUS', 'AUT', 'BEL', 'CAN', 'CHE', 'DEU', 'DNK', 'ESP', 'FIN', 'FRA', 'GBR', 'GRC', 'HKG', 'IRL',
+                     'ISR', 'ITA', 'JPN', 'NLD', 'NOR', 'NZL', 'PRT', 'SGP', 'SWE', 'USA', 'WLD']
+        print(
+            f"AQR publishes excess returns data for the following equity market indexes: {eqty_idxs}"
+        )
+
+    def get_assets_info(self) -> None:
+        """
+        Get assets info.
+        """
+        print(
+            f"AQR does not publish data for individual assets."
+        )
+
+    def get_markets_info(self) -> None:
+        """
+        Get markets info.
+        """
+        print(
+            f"AQR does not publish data for individual markets."
+        )
+
+    @staticmethod
+    def get_fields_info(
+            data_type: Optional[str] = "market", cat: Optional[str] = None
+    ) -> Dict[str, List[str]]:
+        """
+        Get fields info.
+
+        Parameters
+        ----------
+        data_type: str, {'market', 'on-chain', 'off-chain'}, default 'market'
+            Type of data.
+        cat: str, {'crypto', 'eqty', 'fx', 'rates', 'cmdty', 'macro'}, optional, default None
+            Asset class or time series category.
+
+        Returns
+        -------
+        fields: dictionary
+            Dictionary with info on available fields, by category.
+        """
+        if data_type == "on-chain" or data_type == 'off-chain':
+            raise ValueError(
+                "AQR publishes total and excess return series used in their research papers."
+            )
+
+        # list of fields
+        market_fields_list = ['ret', 'tr', 'er']
+
+        # fields dict
+        fields = {
+            "fx": market_fields_list,
+            "rates": market_fields_list,
+            "eqty": market_fields_list,
+            "cmdty": market_fields_list,
+            "credit": market_fields_list,
+        }
+
+        # fields obj
+        if cat is not None:
+            fields = fields[cat]
+
+        return fields
+
+    def get_series(self, data_req: DataRequest) -> Dict[str, pd.DataFrame]:
+        """
+        Gets series from AQR data file.
+
+        Parameters
+        ----------
+        data_req: DataRequest
+            Parameters of data request in CryptoDataPy format.
+
+        Returns
+        -------
+        dfs_dict: dictionary
+            Dictionary with ticker-dataframe key-value pairs.
+
+        """
+        # convert data request parameters to source format
+        conv_data_req = getattr(ConvertParams(data_req), f"to_aqr")()
+
+        try:
+            # fetch data
+            df_dicts = {}
+            for ticker in conv_data_req['tickers']:
+                # create url and sheet names
+                file = conv_data_req['tickers'][ticker][0]  # file name
+                freq = conv_data_req['freq']  # freq
+                ff = self.file_formats[0]  # file format
+                sheet = conv_data_req['tickers'][ticker][1]  # sheet name
+                url = self.base_url + file + freq + "." + ff  # url
+                # read data
+                if file == 'Century-of-Factor-Premia-':
+                    df1 = pd.read_excel(url, sheet_name=sheet, index_col='Unnamed: 0', parse_dates=True, header=18)
+                elif file == 'Time-Series-Momentum-Factors-':
+                    df1 = pd.read_excel(url, sheet_name=sheet, index_col='Unnamed: 0', parse_dates=True, header=17)
+                elif file == 'Commodities-for-the-Long-Run-Index-Level-Data-':
+                    df1 = pd.read_excel(url, sheet_name=sheet, index_col='Unnamed: 0', parse_dates=True, header=10)
+                elif file == 'Credit-Risk-Premium-Preliminary-Paper-Data':
+                    url = self.base_url + file + "." + ff
+                    df1 = pd.read_excel(url, sheet_name=sheet, index_col='Date', parse_dates=True, header=10)
+                else:
+                    df1 = pd.read_excel(url, sheet_name=sheet, index_col='DATE', parse_dates=True, header=18)
+
+                # add df to dicts
+                df_dicts[ticker] = df1
+
+        except Exception as e:
+            logging.warning(e)
+            logging.warning(f"Failed to get data for: {conv_data_req['tickers']}.")
+
+        else:
+            return df_dicts
+
+    @staticmethod
+    def wrangle_data_resp(
+            data_req: DataRequest, data_resp: Dict[str, pd.DataFrame]) -> pd.DataFrame:
+        """
+        Wrangle data response.
+
+        Parameters
+        ----------
+        data_req: DataRequest
+            Parameters of data request in CryptoDataPy format.
+        data_resp: Dictionary
+            Dictionary with ticker-dataframe key-value pairs.
+
+        Returns
+        -------
+        df: pd.DataFrame
+            Wrangled dataframe with DatetimeIndex (level 0), ticker (level 1), and values for market or macro series
+            for selected fields (cols), in tidy format.
+        """
+        # wrangle data resp
+        df = getattr(WrangleData(data_req, data_resp), data_req.source)()
+
+        return df
+
+    def get_tidy_data(self, data_req: DataRequest) -> pd.DataFrame:
+        """
+        Gets data from FRED and wrangles the data response into tidy data format.
+
+        Parameters
+        ----------
+        data_req: DataRequest
+            Parameters of data request in CryptoDataPy format.
+
+        Returns
+        -------
+        df: pd.DataFrame - MultiIndex
+            Dataframe with DatetimeIndex (level 0), tickers (level 1) and actual values (cols),
+            in tidy data format.
+        """
+        # change to get series
+        data_resp = self.get_series(data_req)
+        # wrangle data resp
+        df = self.wrangle_data_resp(data_req, data_resp)
+
+        return df
+
+    def check_params(self, data_req: DataRequest) -> None:
+        """
+        Checks the data request parameters before requesting data to reduce API calls
+        and improve efficiency.
+
+        """
+        # check cat
+        if data_req.cat not in self.categories:
+            raise ValueError(
+                f"Select a valid category. Valid categories are: {self.categories}."
+            )
+        # check freq
+        if data_req.freq not in self.frequencies[data_req.cat]:
+            raise ValueError(
+                f"Invalid data frequency. Valid data frequencies are: {self.frequencies}."
+            )
+        # check fields
+        if not any(field in self.fields[data_req.cat] for field in data_req.fields):
+            raise ValueError(f"Invalid fields. Valid data fields are: {self.fields}.")
+
+    def get_data(self, data_req: DataRequest) -> pd.DataFrame:
+        """
+        Get data.
+
+        Parameters
+        data_req: DataRequest
+            Parameters of data request in CryptoDataPy format.
+
+        Returns
+        -------
+        df: pd.DataFrame - MultiIndex
+            DataFrame with DatetimeIndex (level 0), ticker (level 1), and values for selected fields (cols),
+            in tidy format.
+        """
+        # check params
+        self.check_params(data_req)
+
+        # get tidy data
+        df = self.get_tidy_data(data_req)
+
+        # check if df empty
+        if df.empty:
+            raise Exception(
+                "No data returned. Check data request parameters and try again."
+            )
+
+        return df.sort_index()
---
+++ src/cryptodatapy/extract/web/web.py
@@ -0,0 +1,318 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Optional, Union
+
+import pandas as pd
+
+from cryptodatapy.extract.datarequest import DataRequest
+
+
+class Web(ABC):
+    """
+    Web is an abstract base class which provides a blueprint for properties and methods for the
+    web subclass.
+    """
+
+    def __init__(
+        self,
+        categories,
+        assets,
+        indexes,
+        markets,
+        market_types,
+        fields,
+        frequencies,
+        base_url,
+        file_formats
+
+    ):
+        self.categories = categories
+        self.assets = assets
+        self.indexes = indexes
+        self.markets = markets
+        self.market_types = market_types
+        self.fields = fields
+        self.frequencies = frequencies
+        self.base_url = base_url
+        self.file_formats = file_formats
+
+    @property
+    def categories(self):
+        """
+        Returns a list of available categories for the data vendor.
+        """
+        return self._categories
+
+    @categories.setter
+    def categories(self, categories: Union[str, List[str]]):
+        """
+        Sets a list of available categories for the data vendor.
+        """
+        valid_categories = [
+            None,
+            "crypto",
+            "fx",
+            "eqty",
+            "cmdty",
+            "index",
+            "rates",
+            "bonds",
+            "credit",
+            "macro",
+            "alt",
+        ]
+        cat = []
+
+        if categories is None:
+            self._categories = categories
+        else:
+            if not isinstance(categories, str) and not isinstance(categories, list):
+                raise TypeError("Categories must be a string or list of strings.")
+            if isinstance(categories, str):
+                categories = [categories]
+            for category in categories:
+                if category in valid_categories:
+                    cat.append(category)
+                else:
+                    raise ValueError(
+                        f"{category} is invalid. Valid categories are: {valid_categories}"
+                    )
+            self._categories = cat
+
+    @property
+    def indexes(self):
+        """
+        Returns a list of available indices from the web page.
+        """
+        return self._indexes
+
+    @indexes.setter
+    def indexes(self, indexes: Optional[Union[str, List[str], Dict[str, List[str]]]]):
+        """
+        Sets a list of available indexes for the web page.
+        """
+        if indexes is None or isinstance(indexes, list) or isinstance(indexes, dict):
+            self._indexes = indexes
+        elif isinstance(indexes, str):
+            self._indexes = [indexes]
+        else:
+            raise TypeError(
+                "Indexes must be a string (ticker), list of strings (tickers) or"
+                " a dict with {cat: List[str]} key-value pairs."
+            )
+
+    @abstractmethod
+    def get_indexes_info(self):
+        """
+        Gets info for available indexes from the data vendor.
+        """
+        # to be implemented by subclasses
+
+    @property
+    def assets(self):
+        """
+        Returns a list of available assets for the data vendor.
+        """
+        return self._assets
+
+    @assets.setter
+    def assets(self, assets: Optional[Union[str, List[str], Dict[str, List[str]]]]):
+        """
+        Sets a list of available assets for the data vendor.
+        """
+        if assets is None or isinstance(assets, list) or isinstance(assets, dict):
+            self._assets = assets
+        elif isinstance(assets, str):
+            self._assets = [assets]
+        else:
+            raise TypeError(
+                "Assets must be a string (ticker), list of strings (tickers) or"
+                " a dict with {cat: List[str]} key-value pairs."
+            )
+
+    @abstractmethod
+    def get_assets_info(self):
+        """
+        Gets info for available assets from the data vendor.
+        """
+        # to be implemented by subclasses
+
+    @property
+    def markets(self):
+        """
+        Returns a list of available markets for the data vendor.
+        """
+        return self._markets
+
+    @markets.setter
+    def markets(self, markets: Optional[Union[str, List[str], Dict[str, List[str]]]]):
+        """
+        Sets a list of available markets for the data vendor.
+        """
+        if markets is None or isinstance(markets, list) or isinstance(markets, dict):
+            self._markets = markets
+        elif isinstance(markets, str):
+            self._markets = [markets]
+        else:
+            raise TypeError(
+                "Markets must be a string (ticker), list of strings (tickers) or"
+                " a dict with {cat: List[str]} key-value pairs."
+            )
+
+    @abstractmethod
+    def get_markets_info(self):
+        """
+        Gets info for available markets from the data vendor.
+        """
+        # to be implemented by subclasses
+
+    @property
+    def market_types(self):
+        """
+        Returns a list of available market types for the data vendor.
+        """
+        return self._market_types
+
+    @market_types.setter
+    def market_types(self, market_types: Optional[Union[str, List[str]]]):
+        """
+        Sets a list of available market types for the data vendor.
+        """
+        valid_mkt_types, mkt_types_list = [
+            None,
+            "spot",
+            "etf",
+            "perpetual_future",
+            "future",
+            "swap",
+            "option",
+        ], []
+
+        if market_types is None:
+            self._market_types = market_types
+        elif isinstance(market_types, str) and market_types in valid_mkt_types:
+            self._market_types = [market_types]
+        elif isinstance(market_types, list):
+            for mkt in market_types:
+                if mkt in valid_mkt_types:
+                    mkt_types_list.append(mkt)
+                else:
+                    raise ValueError(
+                        f"{mkt} is invalid. Valid market types are: {valid_mkt_types}"
+                    )
+            self._market_types = mkt_types_list
+        else:
+            raise TypeError("Market types must be a string or list of strings.")
+
+    @property
+    def fields(self):
+        """
+        Returns a list of available fields for the data vendor.
+        """
+        return self._fields
+
+    @fields.setter
+    def fields(self, fields: Optional[Union[str, List[str], Dict[str, List[str]]]]):
+        """
+        Sets a list of available fields for the data vendor.
+        """
+        if fields is None or isinstance(fields, list) or isinstance(fields, dict):
+            self._fields = fields
+        elif isinstance(fields, str):
+            self._fields = [fields]
+        else:
+            raise TypeError(
+                "Fields must be a string (field), list of strings (fields) or"
+                " a dict with {cat: List[str]} key-value pairs."
+            )
+
+    @abstractmethod
+    def get_fields_info(self, data_type: Optional[str]):
+        """
+        Gets info for available fields from the data vendor.
+        """
+        # to be implemented by subclasses
+
+    @property
+    def frequencies(self):
+        """
+        Returns a list of available data frequencies for the data vendor.
+        """
+        return self._frequencies
+
+    @frequencies.setter
+    def frequencies(
+        self, frequencies: Optional[Union[str, List[str], Dict[str, List[str]]]]
+    ):
+        """
+        Sets a list of available data frequencies for the data vendor.
+        """
+        if (
+            frequencies is None
+            or isinstance(frequencies, list)
+            or isinstance(frequencies, dict)
+        ):
+            self._frequencies = frequencies
+        elif isinstance(frequencies, str):
+            self._frequencies = [frequencies]
+        else:
+            raise TypeError(
+                "Frequencies must be a string (frequency), list of strings (frequencies) or"
+                " a dict with {cat: List[str]} key-value pairs."
+            )
+
+    @property
+    def base_url(self):
+        """
+        Returns the base url for the data vendor.
+        """
+        return self._base_url
+
+    @base_url.setter
+    def base_url(self, url: Optional[str]):
+        """
+        Sets the base url for the data vendor.
+        """
+        if url is None or isinstance(url, str):
+            self._base_url = url
+        else:
+            raise TypeError(
+                "Base url must be a string containing the data vendor's base URL to which endpoint paths"
+                " are appended."
+            )
+
+    @property
+    def file_formats(self):
+        """
+        Returns the file formats for the files on the web page.
+        """
+        return self._file_formats
+
+    @file_formats.setter
+    def file_formats(self, formats: Optional[Union[str, List[str]]]):
+        """
+        Sets the file formats for the files on the web page.
+        """
+        if formats is None or isinstance(formats, list):
+            self._file_formats = formats
+        elif isinstance(formats, str):
+            self._file_formats = [formats]
+        else:
+            raise TypeError(
+                "File format must be a string or list containing the file formats for the web page's data files"
+            )
+
+    @abstractmethod
+    def get_data(self, data_req: DataRequest) -> pd.DataFrame:
+        """
+        Submits get data request to API.
+        """
+        # to be implemented by subclasses
+
+    @staticmethod
+    @abstractmethod
+    def wrangle_data_resp(data_req: DataRequest, data_resp: Union[Dict[str, Any], pd.DataFrame], **kwargs) \
+            -> pd.DataFrame:
+        """
+        Wrangles data response from data vendor API into tidy format.
+        """
+        # to be implemented by subclasses

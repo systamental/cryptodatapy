5e70ebdce1b6754725c5c2cbc6cea13027fa6910
Nick Latinovic <nicklatinovic@gmail.com>
2024-08-08 22:50:09 +0200

    fix: refactored impute module and testing

--- src/cryptodatapy/transform/impute.py
+++ src/cryptodatapy/transform/impute.py
@@ -7,67 +7,51 @@ import pandas as pd
 class Impute:
     """
     Handles missing values.
-
     """
-
-    def __init__(self, filt_df: pd.DataFrame):
-
+    def __init__(self, filtered_df: pd.DataFrame, plot: bool = False, plot_series: tuple = ("BTC", "close")):
         """
         Constructor
 
         Parameters
         ----------
-        filt_df: pd.DataFrame - MultiIndex
+        filtered_df: pd.DataFrame - MultiIndex
             DataFrame MultiIndex with DatetimeIndex (level 0), ticker (level 1) and fields (cols) with filtered values.
-
         """
-        self.filt_df = filt_df
+        self.filtered_df = filtered_df.astype(float)
+        self.plot = plot
+        self.plot_series = plot_series
+        self.imputed_df = None
 
-    def fwd_fill(
-        self, plot: bool = False, plot_series: tuple = ("BTC", "close")
-    ) -> pd.DataFrame:
+    def fwd_fill(self) -> pd.DataFrame:
         """
         Imputes missing values by imputing missing values with latest non-missing values.
 
-        Parameters
-        ----------
-        plot: bool, default False
-            Plots series with outliers highlighted with red dots.
-        plot_series: tuple, default ('BTC', 'close')
-            Plots the time series of a specific (ticker, field/column) tuple.
-
         Returns
         -------
-        imp_df: pd.DataFrame - MultiIndex
+        imputed_df: pd.DataFrame - MultiIndex
             DataFrame MultiIndex with DatetimeIndex (level 0), ticker (level 1) and fields (cols) with imputed values
             using forward fill method.
-
         """
-        # copy df
-        filt_df = self.filt_df.copy()
-
         # ffill
-        imp_df = filt_df.groupby(level=1).ffill()
+        self.imputed_df = self.filtered_df.groupby(level=1).ffill()
 
         # plot
-        if plot:
-            if not isinstance(plot_series, tuple):
+        if self.plot:
+            if not isinstance(self.plot_series, tuple):
                 raise TypeError(
                     "Plot_series must be a tuple specifying the ticker and column/field to plot (ticker, column)."
                 )
             else:
-                self.plot_imputed(imp_df, plot_series=plot_series)
+                self.plot_imputed()
 
-        return imp_df
+        return self.imputed_df
 
     def interpolate(
         self,
         method: str = "linear",
         order: Optional[int] = None,
-        axis=0,
+        axis: int = 0,
         limit: Optional[int] = None,
-        plot: bool = False,
-        plot_series: tuple = ("BTC", "close"),
     ) -> pd.DataFrame:
         """
         Imputes missing values by interpolating using various methods.
@@ -83,116 +67,85 @@ class Impute:
             Axis to interpolate along.
         limit: int, optional, default None
             Maximum number of consecutive NaNs to fill. Must be greater than 0.
-        plot: bool, default False
-            Plots series with outliers highlighted with red dots.
-        plot_series: tuple, default ('BTC', 'close')
-            Plots the time series of a specific (ticker, field/column) tuple.
 
         Returns
         -------
-        imp_df: pd.DataFrame - MultiIndex
+        imputed_df: pd.DataFrame - MultiIndex
             DataFrame MultiIndex with DatetimeIndex (level 0), ticker (level 1) and fields (cols) with imputed values
             using interpolation method.
-
         """
-        # copy df and convert to float for interpolation (code will break if type int64)
-        filt_df = self.filt_df.astype(float).copy()
-
         # add order if spline or polynomial
         if (method == "spline" or method == "polynomial") and order is None:
             order = 3
 
         # interpolate
-        imp_df = (
-            filt_df.unstack()
-            .interpolate(method=method, order=order, axis=axis, limit=limit)
-            .stack()
-            .reindex(filt_df.index)
-        )
+        self.imputed_df = self.filtered_df.unstack().interpolate(method=method, order=order, axis=axis,
+                                                                 limit=limit).stack().reindex(self.filtered_df.index)
 
         # type conversion
-        imp_df = imp_df.apply(pd.to_numeric, errors="ignore").convert_dtypes()
+        self.imputed_df = self.imputed_df.apply(pd.to_numeric, errors="ignore").convert_dtypes()
 
         # plot
-        if plot:
-            if not isinstance(plot_series, tuple):
+        if self.plot:
+            if not isinstance(self.plot_series, tuple):
                 raise TypeError(
                     "Plot_series must be a tuple specifying the ticker and column/field to plot (ticker, column)."
                 )
             else:
-                self.plot_imputed(imp_df, plot_series=plot_series)
+                self.plot_imputed()
 
-        return imp_df
+        return self.imputed_df
 
     def fcst(
         self,
-        fcst_df: pd.DataFrame,
-        plot: bool = False,
-        plot_series: tuple = ("BTC", "close"),
+        yhat_df: pd.DataFrame,
     ) -> pd.DataFrame:
         """
         Imputes missing values with forecasts from outlier detection algorithm.
 
         Parameters
         ----------
-        fcst_df: pd.DataFrame - MultiIndex
+        yhat_df: pd.DataFrame - MultiIndex
             Multiindex dataframe with DatetimeIndex (level 0), tickers (level 1) and fields (cols)
             with forecasted values.
-        plot: bool, default False
-            Plots series with outliers highlighted with red dots.
-        plot_series: tuple, default ('BTC', 'close')
-            Plots the time series of a specific (ticker, field/column) tuple.
 
         Returns
         -------
-        imp_df: pd.DataFrame - MultiIndex
+        imputed_df: pd.DataFrame - MultiIndex
             DataFrame MultiIndex with DatetimeIndex (level 0), ticker (level 1) and fields (cols) with imputed values
             using forecasts from outlier detection method.
-
         """
-        # copy filtered and forecast dfs
-        filt_df, yhat_df = self.filt_df.copy(), fcst_df.copy()
-
         # impute missing vals in filtered df with fcst vals
-        imp_yhat = np.where(filt_df.isna(), yhat_df, filt_df)
+        imp_yhat = np.where(self.filtered_df.isna(), yhat_df, self.filtered_df)
         # create df
-        imp_df = pd.DataFrame(imp_yhat, index=filt_df.index, columns=filt_df.columns)
+        self.imputed_df = pd.DataFrame(imp_yhat, index=self.filtered_df.index, columns=self.filtered_df.columns)
 
         # type conversion
-        imp_df = imp_df.apply(pd.to_numeric, errors="ignore").convert_dtypes()
+        self.imputed_df = self.imputed_df.apply(pd.to_numeric, errors="ignore").convert_dtypes()
 
         # plot
-        if plot:
-            if not isinstance(plot_series, tuple):
+        if self.plot:
+            if not isinstance(self.plot_series, tuple):
                 raise TypeError(
                     "Plot_series must be a tuple specifying the ticker and column/field to plot (ticker, column)."
                 )
             else:
-                self.plot_imputed(imp_df, plot_series=plot_series)
+                self.plot_imputed()
 
-        return imp_df
+        return self.imputed_df
 
-    @staticmethod
-    def plot_imputed(imp_df: pd.DataFrame, plot_series: Optional[tuple] = None) -> None:
+    def plot_imputed(self) -> None:
         """
         Plots filtered time series.
-
-        Parameters
-        ----------
-        imp_df: pd.DataFrame - MultiIndex
-            DataFrame MultiIndex with DatetimeIndex (level 0), ticker (level 1) and fields (cols) with imputed values.
-        plot_series: tuple, optional, default None
-            Plots the time series of a specific (ticker, field) tuple.
-
         """
         ax = (
-            imp_df.loc[pd.IndexSlice[:, plot_series[0]], plot_series[1]]
+            self.imputed_df.loc[pd.IndexSlice[:, self.plot_series[0]], self.plot_series[1]]
             .droplevel(1)
             .plot(linewidth=1, figsize=(15, 7), color="#1f77b4", zorder=0)
         )
         ax.grid(color="black", linewidth=0.05)
         ax.xaxis.grid(False)
-        ax.set_ylabel(plot_series[0])
+        ax.set_ylabel(self.plot_series[0])
         ax.ticklabel_format(style="plain", axis="y")
         ax.set_facecolor("whitesmoke")
-        ax.legend([plot_series[1] + "_repaired"], loc="upper left")
+        ax.legend([self.plot_series[1] + "_repaired"], loc="upper left")
--- tests/test_impute.py
+++ tests/test_impute.py
@@ -2,98 +2,105 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from cryptodatapy.transform.filter import Filter
 from cryptodatapy.transform.impute import Impute
 from cryptodatapy.transform.od import OutlierDetection
 
-# get data for testing
+
 @pytest.fixture
 def raw_oc_data():
     return pd.read_csv('data/cm_raw_oc_df.csv', index_col=[0, 1], parse_dates=['date'])
 
+
 @pytest.fixture
-def oc_filt_df(raw_oc_data):
-    outliers_dict = OutlierDetection(raw_oc_data).stl(thresh_val=10)
-    filt_df = Filter(raw_oc_data).outliers(outliers_dict)
-    return filt_df
+def filtered_data(raw_oc_data):
+    od = OutlierDetection(raw_oc_data, thresh_val=10)
+    od.stl()
+    return od.filtered_df
 
 
-def test_impute_fwd_fill(oc_filt_df, raw_oc_data) -> None:
-    """
-    Test filter average trading value below threshold.
-    """
-    # impute
-    imp_df = Impute(oc_filt_df).fwd_fill()
-    # assert statements
-    assert imp_df.shape == raw_oc_data.shape, "Filtered dataframe changed shape."  # shape
-    assert isinstance(
-        imp_df.index, pd.MultiIndex
-    ), "Dataframe should be multiIndex."  # multiindex
-    assert isinstance(
-        imp_df.index.droplevel(1), pd.DatetimeIndex
-    ), "Index is not DatetimeIndex."  # datetimeindex
-    assert all(
-        oc_filt_df.isna().sum() > imp_df.isna().sum()
-    ), "Some missing values were not imputed."  # imp vals
-    assert not any(
-        (imp_df.describe().loc["max"] == np.inf)
-        & (imp_df.describe().loc["min"] == -np.inf)
-    ), "Inf values found in the dataframe"  # inf
-    assert isinstance(
-        imp_df.close.dropna().iloc[-1], np.float64
-    ), "Imputed close is not a numpy float."  # dtypes
+@pytest.fixture
+def yhat_data(raw_oc_data):
+    od = OutlierDetection(raw_oc_data, thresh_val=10)
+    od.stl()
+    return od.yhat
 
 
-def test_impute_interpolate(oc_filt_df, raw_oc_data) -> None:
+class TestImpute:
     """
-    Test filter average trading value below threshold.
+    Test class for Impute.
     """
-    # impute
-    imp_df = Impute(oc_filt_df).interpolate()
-    # assert statements
-    assert imp_df.shape == raw_oc_data.shape, "Filtered dataframe changed shape."  # shape
-    assert isinstance(
-        imp_df.index, pd.MultiIndex
-    ), "Dataframe should be multiIndex."  # multiindex
-    assert isinstance(
-        imp_df.index.droplevel(1), pd.DatetimeIndex
-    ), "Index is not DatetimeIndex."  # datetimeindex
-    assert all(
-        oc_filt_df.isna().sum() > imp_df.isna().sum()
-    ), "Some missing values were not imputed."  # imp vals
-    assert not any(
-        (imp_df.describe().loc["max"] == np.inf)
-        & (imp_df.describe().loc["min"] == -np.inf)
-    ), "Inf values found in the dataframe"  # inf
-    assert isinstance(
-        imp_df.close.dropna().iloc[-1], np.float64
-    ), "Imputed close is not a numpy float."  # dtypes
+    @pytest.fixture(autouse=True)
+    def imp_instance(self, filtered_data):
+        self.imp_instance = Impute(filtered_data)
 
+    def test_initialization(self) -> None:
+        """
+        Test initialization.
+        """
+        # types
+        assert isinstance(self.imp_instance, Impute)
+        assert isinstance(self.imp_instance.filtered_df, pd.DataFrame)
+        assert (self.imp_instance.filtered_df.dtypes == 'float64').all()
 
-def test_impute_fcst(oc_filt_df, raw_oc_data) -> None:
-    """
-    Test filter average trading value below threshold.
-    """
-    # impute
-    imp_df = Impute(oc_filt_df).interpolate()
-    # assert statements
-    assert imp_df.shape == raw_oc_data.shape, "Filtered dataframe changed shape."  # shape
-    assert isinstance(
-        imp_df.index, pd.MultiIndex
-    ), "Dataframe should be multiIndex."  # multiindex
-    assert isinstance(
-        imp_df.index.droplevel(1), pd.DatetimeIndex
-    ), "Index is not DatetimeIndex."  # datetimeindex
-    assert all(
-        oc_filt_df.isna().sum() > imp_df.isna().sum()
-    ), "Some missing values were not imputed."  # imp vals
-    assert not any(
-        (imp_df.describe().loc["max"] == np.inf)
-        & (imp_df.describe().loc["min"] == -np.inf)
-    ), "Inf values found in the dataframe"  # inf
-    assert isinstance(
-        imp_df.close.dropna().iloc[-1], np.float64
-    ), "Imputed close is not a numpy float."  # dtypes
+    def test_impute_fwd_fill(self, raw_oc_data) -> None:
+        """
+        Test impute missing values with forward fill.
+        """
+        # impute
+        self.imp_instance.fwd_fill()
+
+        # assert statements
+        assert self.imp_instance.imputed_df.shape == self.imp_instance.filtered_df.shape, \
+            "Filtered dataframe changed shape."
+        assert isinstance(self.imp_instance.imputed_df.index, pd.MultiIndex), "Dataframe should be multiIndex."
+        assert isinstance(self.imp_instance.imputed_df.index.droplevel(1), pd.DatetimeIndex), \
+            "Index is not DatetimeIndex."
+        assert all(self.imp_instance.filtered_df.isna().sum() > self.imp_instance.imputed_df.isna().sum()), \
+            "Some missing values were not imputed."
+        assert not any((self.imp_instance.imputed_df.describe().loc['max'] == np.inf) &
+                       (self.imp_instance.imputed_df.describe().loc['min'] == -np.inf)), \
+            "Inf values found in the dataframe"
+        assert (self.imp_instance.imputed_df.dtypes == 'float64').all(), "Imputed close is not a float."
+
+    def test_impute_interpolate(self, raw_oc_data) -> None:
+        """
+        Test impute missing values with interpolation.
+        """
+        # impute
+        self.imp_instance.interpolate()
+
+        # assert statements
+        assert self.imp_instance.imputed_df.shape == self.imp_instance.filtered_df.shape, \
+            "Filtered dataframe changed shape."
+        assert isinstance(self.imp_instance.imputed_df.index, pd.MultiIndex), "Dataframe should be multiIndex."
+        assert isinstance(self.imp_instance.imputed_df.index.droplevel(1), pd.DatetimeIndex), \
+            "Index is not DatetimeIndex."
+        assert all(self.imp_instance.filtered_df.isna().sum() > self.imp_instance.imputed_df.isna().sum()), \
+            "Some missing values were not imputed."
+        assert not any((self.imp_instance.imputed_df.describe().loc['max'] == np.inf) &
+                       (self.imp_instance.imputed_df.describe().loc['min'] == -np.inf)), \
+            "Inf values found in the dataframe"
+        assert (self.imp_instance.imputed_df.dtypes == 'Float64').all(), "Imputed close is not a float."
+
+    def test_impute_fcst(self, yhat_data) -> None:
+        """
+        Test impute missing values with forecasts.
+        """
+        # impute
+        self.imp_instance.fcst(yhat_data)
+
+        # assert statements
+        assert self.imp_instance.imputed_df.shape == self.imp_instance.filtered_df.shape, \
+            "Filtered dataframe changed shape."
+        assert isinstance(self.imp_instance.imputed_df.index, pd.MultiIndex), "Dataframe should be multiIndex."
+        assert isinstance(self.imp_instance.imputed_df.index.droplevel(1), pd.DatetimeIndex), \
+            "Index is not DatetimeIndex."
+        assert all(self.imp_instance.filtered_df.isna().sum() > self.imp_instance.imputed_df.isna().sum()), \
+            "Some missing values were not imputed."
+        assert not any((self.imp_instance.imputed_df.describe().loc['max'] == np.inf) &
+                       (self.imp_instance.imputed_df.describe().loc['min'] == -np.inf)), \
+            "Inf values found in the dataframe"
+        assert (self.imp_instance.imputed_df.dtypes == 'Float64').all(), "Imputed close is not a float."
 
 
 if __name__ == "__main__":

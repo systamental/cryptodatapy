486ab8c3691b66747525c449b2f4510955951590
Nick Latinovic <nicklatinovic@gmail.com>
2026-01-04 23:47:30 +0100

    feat: added defillama to data sources dict

--- src/cryptodatapy/extract/datarequest.py
+++ src/cryptodatapy/extract/datarequest.py
@@ -130,6 +130,7 @@ class DataRequest:
             "cryptocompare",
             "coinmetrics",
             "ccxt",
+            'defillama',
             "dydx",
             "glassnode",
             "tiingo",
